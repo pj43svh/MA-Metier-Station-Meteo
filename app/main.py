6535a2cfd1a022633ee4ce98ea494ac016d97962
Amin Torrisi <amin.torrisi@hotmail.com>
2026-01-28 10:47:25 +0100

    Fix module imports for Railway deployment

--- app/main.py
+++ app/main.py
@@ -8,9 +8,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-from api import api
-from route import route
-from esp import esp
+from app.api import api
+from app.route import route
+from app.esp import esp
 
 # Les blueprints serevnt à séparer les différentes parties de l'application
 # Quand on va sur /api/quelquechose, ça va aller dans api.py puis sur les
